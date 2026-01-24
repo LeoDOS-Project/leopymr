37b1e3aaea9a82f28fdd58f5b0687ecb93f32c5f
Thomas Sandholm <tom@patompa.com>
2026-01-23 20:19:01 -0800

    add test

---
+++ run_test.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+BENCHES="random bipartite"
+echo "BENCH VAL ITER" > result.dat
+for ITER in `seq 1 10`; do
+for BENCH in $BENCHES; do
+  ./up.sh test
+  sleep 15
+  python3 submit.py $BENCH
+  sleep 15
+  VAL=`./job_time.sh`
+  echo "$BENCH $VAL $ITER" >> result.dat
+done
+done
