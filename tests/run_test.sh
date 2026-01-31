b11a07da39142d2bdb7948b2fa5f249130165eae
Thomas Sandholm <tom@patompa.com>
2026-01-30 23:00:54 -0800

    test timing

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -6,12 +6,13 @@ PLACEMENTS="los center"
 ITERATIONS=${ITERATIONS:-1}
 echo "BENCH PLACEMENT DISTANCE REDUCEDISTANCE VAL ITER" > result.dat
 MAX_SAT=11 MAX_ORB=11 ../create_constellation.sh test
+cat ../composetest.yaml
 for ITER in `seq 1 $ITERATIONS`; do
 SEED=$RANDOM
 for BENCH in $BENCHES; do
   for PLACEMENT in $PLACEMENTS; do
   ../up.sh test
-  sleep 10
+  sleep 20
   python3 ../job.py -a $BENCH -s $SEED -r ${PLACEMENT} -mi 3 -ma 8 -c 15 > submit.json
   DIST=`cat submit.json | jq .distance`
   REDDIST=`cat submit.json | jq .reduce_distance`
