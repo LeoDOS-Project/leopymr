c9f165b830f7e761f8210bb5c1a7c3d3c6019384
Thomas Sandholm <tom@patompa.com>
2026-02-02 07:07:12 -0800

    fix down script

--- down.sh
+++ down.sh
@@ -2,4 +2,4 @@
 DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd ${DIR}
 NAME=$1
-docker compose -f compose${NAME}.yaml down
+docker compose -f compose${NAME}.yaml down --remove-orphans
