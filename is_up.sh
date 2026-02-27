b85fe2cdb5dd3f4ab8f228af1993f607257286fc
Thomas Sandholm <tom@patompa.com>
2026-02-27 14:21:57 +0100

    up script fix

--- is_up.sh
+++ is_up.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 RT=$1
-ORBS=`cat compose${1}.yaml  | grep MAX_ORB | uniq | awk '{print $2}' | sed 's/"//g'`
-SATS=`cat compose${1}.yaml  | grep MAX_SAT | uniq | awk '{print $2}' | sed 's/"//g'`
+ORBS=`cat compose${RT}.yaml  | grep MAX_ORB | uniq | awk '{print $2}' | sed 's/"//g'`
+SATS=`cat compose${RT}.yaml  | grep MAX_SAT | uniq | awk '{print $2}' | sed 's/"//g'`
 EXPECTED=`echo "$ORBS * $SATS + 1" | bc`
 CURRENT=`./logs.sh $RT | grep "Press CTRL+C" | wc -l | awk '{print $1}'`
 echo "Expected $EXPECTED Current $CURRENT"
